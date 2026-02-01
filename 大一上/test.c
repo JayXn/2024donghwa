fd85d1a8718500a174a4cc86c58e1092935142ea
JayXn <xja940129@gmail.com>
2026-02-01 14:36:16 +0800

    Add 大一上 (originally 2023programming)

---
+++ 大一上/test.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    char a = "123";
+    char* c = a;
+    int b = (int)c; q
+    printf("%d", b);
+
+    return 0;
+}
+
